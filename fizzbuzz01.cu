5cf6569fbe102f16e3eeacdbc22669a2660b9f38
Sunao Komuro <konbu.komuro@gmail.com>
2010-08-08 20:55:41 +0900

    fix num

--- fizzbuzz01.cu
+++ fizzbuzz01.cu
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #define COUNT 40
 
 enum FizzBuzzEnum {
@@ -9,8 +10,8 @@ __device__ FizzBuzzEnum to_enum(int num) {
     return (num%15)==0 ? FIZZBUZZ : (num%3)==0 ? FIZZ : (num%5)==0 ? BUZZ : NONE;
 }
 __global__ void dev_fizzbuzz(FizzBuzzEnum *d_fizzbuzz) {
-    int num = threadIdx.x + 1;
-    d_fizzbuzz[num] = to_enum(num);
+    int i = threadIdx.x;
+    d_fizzbuzz[i] = to_enum(i+1);
 }
 
 int main() {
@@ -37,6 +38,8 @@ int main() {
 	    case NONE:
 		printf("%d\n", i+1);
 		break;
+	    default:
+		assert(0);
 	}
     }
 
