c7310834764b801f0acfac0bef3afb4ff49bd08a
Sunao Komuro <konbu.komuro@gmail.com>
2010-08-08 20:48:49 +0900

    add fizzbuzz01

--- Makefile
+++ Makefile
@@ -2,16 +2,19 @@ SDK= $(CUDA_SDK)
 INCLUDES+= -I$(SDK)/C/common/inc
 LDFLAGS+=-L$(SDK)/C/lib -L$(SDK)/C/common/lib
 
-TARGETS= hello hello_cuda cuda001 cuda002
+TARGETS= hello hello_cuda fizzbuzz01 cuda001 cuda002
 
 all: $(TARGETS)
 clean:
-	rm $(TARGETS)
+	rm -f $(TARGETS)
 
 hello: hello.cu
 	nvcc -o $@ $(INCLUDES) $(LDFLAGS) $^
 
 hello_cuda: hello_cuda.cu
+	nvcc -o $@ $(INCLUDES) $(LDFLAGS) $^
+
+fizzbuzz01: fizzbuzz01.cu
 	nvcc -o $@ $(INCLUDES) $(LDFLAGS) $^
 
 cuda001: cuda001.cu
---
+++ fizzbuzz01.cu
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define COUNT 40
+
+enum FizzBuzzEnum {
+    FIZZBUZZ, FIZZ, BUZZ, NONE
+};
+
+__device__ FizzBuzzEnum to_enum(int num) {
+    return (num%15)==0 ? FIZZBUZZ : (num%3)==0 ? FIZZ : (num%5)==0 ? BUZZ : NONE;
+}
+__global__ void dev_fizzbuzz(FizzBuzzEnum *d_fizzbuzz) {
+    int num = blockIdx.x + 1;
+    d_fizzbuzz[num] = to_enum(num);
+}
+
+int main() {
+    FizzBuzzEnum h_fizzbuzz[COUNT];
+    FizzBuzzEnum *d_fizzbuzz;
+
+    cudaMalloc(&d_fizzbuzz, sizeof(h_fizzbuzz));
+
+    dev_fizzbuzz<<<COUNT,1>>>(d_fizzbuzz);
+
+    cudaMemcpy(h_fizzbuzz, d_fizzbuzz, sizeof(h_fizzbuzz), cudaMemcpyDeviceToHost);
+
+    for (int i=0;i<COUNT;++i) {
+	switch(h_fizzbuzz[i]) {
+	    case FIZZBUZZ:
+		puts("FIZZBUZZ");
+		break;
+	    case FIZZ:
+		puts("FIZZ");
+		break;
+	    case BUZZ:
+		puts("BUZZ");
+		break;
+	    case NONE:
+		printf("%d\n", i+1);
+		break;
+	}
+    }
+
+    cudaFree(d_fizzbuzz);
+
+    return 0;
+}
+// vim: set ft=cpp:
