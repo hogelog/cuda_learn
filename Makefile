1b20796785d3751e7ecc297ecc7e13b33ae77280
cuda <cuda@cuda-desktop.(none)>
2010-08-12 16:57:24 +0900

    add ao_cuda.cu

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ CXX= g++
 TOP= /usr/local/cuda
 SDK= $(CUDA_SDK)
 
+INC=
 CFLAGS+=-g -O3 -Wall
 CXXFLAGS+=-g -O3 -Wall
 NVCCFLAGS+=-g -G -O3
@@ -12,45 +13,37 @@ INC_CUBLAS= -I$(TOP)/include
 LD_CUBLAS= -lcublas -L$(TOP)/lib
 
 INC_CUTIL= -I$(SDK)/C/common/inc
-LD_CUTIL=-lcutil -L$(SDK)/C/lib -L$(SDK)/C/common/lib
+LD_CUTIL=-lcutil_i386 -L$(SDK)/C/lib -L$(SDK)/C/common/lib
 
-TARGETS= hello hello_cuda fizzbuzz01 fizzbuzz02 white2black white2black_cuda cuda001 cuda002 smallpt_cpu smallpt_cuda ao ao_cuda
+TARGETS= hello hello_cuda fizzbuzz01 fizzbuzz02 white2black white2black_cuda cuda001 cuda002 smallpt_cpu ao ao_cuda
 
 all: $(TARGETS)
 clean:
 	rm -f $(TARGETS)
 
 hello: hello.cu
-	nvcc -o $@ $(NVCCFLAGS) $(INCLUDES) $(LDFLAGS) $^
+	nvcc -o $@ $(NVCCFLAGS) $(INC) $(LDFLAGS) $^
 
 hello_cuda: hello_cuda.cu
-	nvcc -o $@ $(NVCCFLAGS) $(INCLUDES) $(LDFLAGS) $^
+	nvcc -o $@ $(NVCCFLAGS) $(INC) $(LDFLAGS) $^
 
 fizzbuzz01: fizzbuzz01.cu
-	nvcc -o $@ $(NVCCFLAGS) $(INCLUDES) $(LDFLAGS) $^
+	nvcc -o $@ $(NVCCFLAGS) $(INC) $(LDFLAGS) $^
 
 fizzbuzz02: fizzbuzz02.cu
-	nvcc -o $@ $(NVCCFLAGS) $(INCLUDES) $(LDFLAGS) $^
+	nvcc -o $@ $(NVCCFLAGS) $(INC) $(LDFLAGS) $^
 
 white2black: white2black.c
 	$(CC) -o $@ $(CFLAGS) -fopenmp $^
 
 white2black_cuda: white2black_cuda.cu
-	nvcc -o $@ $(NVCCFLAGS) $(INCLUDES) $(LDFLAGS) $^
-
-cuda001: cuda001.cu
-	nvcc -o $@ $(LD_CUTIL) $(INC_CUTIL) $(NVCCFLAGS) $(INCLUDES) $(LDFLAGS) $^
-cuda002: cuda002.cu
-	nvcc -o $@ $(LD_CUTIL) $(INC_CUTIL) $(NVCCFLAGS) $(INCLUDES) $(LDFLAGS) $^
+	nvcc -o $@ $(NVCCFLAGS) $(INC) $(LDFLAGS) $^
 
 ao: ao.c
 	$(CC) -o $@ -lm $(CFLAGS) -fopenmp $^
 
 ao_cuda: ao_cuda.cu
-	nvcc -o $@ $(NVCCFLAGS) $(INCLUDES) $(LDFLAGS) $^
+	nvcc -o $@ $(INC_CUTIL) $(NVCCFLAGS) $(INC) $(LDFLAGS) $^
 
 smallpt_cpu: smallpt_cpu.cpp
 	$(CXX) -o $@ $(CXXFLAGS) -fopenmp $^
-
-smallpt_cuda: smallpt_cuda.cu
-	nvcc -o $@ $(NVCCFLAGS) $(INCLUDES) $(LDFLAGS) $^
--- ao_cuda.cu
+++ ao_cuda.cu
@@ -4,68 +4,105 @@
 #include <assert.h>
 #include <math.h>
 
+#include <cutil_inline.h>
+
+#include <thrust/random.h>
+
 #define WIDTH        256
 #define HEIGHT       256
 #define NSUBSAMPLES  2
 #define NAO_SAMPLES  8
 
-struct vec {
-    double x, y, z;
-};
-struct Isect {
-    double t;
-    vec p, n;
-    int hit; 
-};
-struct Sphere {
-    vec center;
-    double radius;
-};
-struct Plane {
-    vec p, n;
-};
-struct Ray {
-    vec org, dir;
+typedef thrust::default_random_engine RNG;
+typedef thrust::uniform_real_distribution<float> RNG_range;
+
+struct vec
+{
+    float x;
+    float y;
+    float z;
 };
 
-static double vdot(vec v0, vec v1)
+
+struct Isect
+{
+    float t;
+    vec    p;
+    vec    n;
+    int    hit; 
+};
+
+struct Sphere
+{
+    vec    center;
+    float radius;
+
+};
+
+struct Plane
+{
+    vec    p;
+    vec    n;
+};
+
+struct Ray
+{
+    vec    org;
+    vec    dir;
+};
+
+__device__ Sphere spheres[3] = {
+    {{-2.0f, 0.0f, -3.5f}, 0.5f},
+    {{-0.5f, 0.0f, -3.0f}, 0.5f},
+    {{1.0f,  0.0f, -2.2f}, 0.5f}};
+__device__ Plane plane = {
+    {0.0f, -0.5f, 0.0f},
+    {0.0f,  1.0f, 0.0f}};
+
+__device__ static float
+vdot(vec v0, vec v1)
 {
     return v0.x * v1.x + v0.y * v1.y + v0.z * v1.z;
 }
 
-static void vcross(vec *c, vec v0, vec v1)
+__device__ static void
+vcross(vec *c, vec v0, vec v1)
 {
+    
     c->x = v0.y * v1.z - v0.z * v1.y;
     c->y = v0.z * v1.x - v0.x * v1.z;
     c->z = v0.x * v1.y - v0.y * v1.x;
 }
 
-static void vnormalize(vec *c)
+__device__ static void
+vnormalize(vec *c)
 {
-    double length = sqrt(vdot((*c), (*c)));
+    float length = sqrt(vdot((*c), (*c)));
 
-    if (fabs(length) > 1.0e-17) {
+    if (fabs(length) > 1.0e-17f) {
         c->x /= length;
         c->y /= length;
         c->z /= length;
     }
 }
 
-void
+__device__ void
 ray_sphere_intersect(Isect *isect, const Ray *ray, const Sphere *sphere)
 {
-    vec rs = {
-        ray->org.x - sphere->center.x,
-        ray->org.y - sphere->center.y,
-        ray->org.z - sphere->center.z};
-    double B = vdot(rs, ray->dir);
-    double C = vdot(rs, rs) - sphere->radius * sphere->radius;
-    double D = B * B - C;
+    vec rs;
 
-    if (D > 0.0) {
-        double t = -B - sqrt(D);
+    rs.x = ray->org.x - sphere->center.x;
+    rs.y = ray->org.y - sphere->center.y;
+    rs.z = ray->org.z - sphere->center.z;
+
+    float B = vdot(rs, ray->dir);
+    float C = vdot(rs, rs) - sphere->radius * sphere->radius;
+    float D = B * B - C;
+
+    if (D > 0.0f) {
+        float t = -B - sqrt(D);
         
-        if ((t > 0.0) && (t < isect->t)) {
+        if ((t > 0.0f) && (t < isect->t)) {
             isect->t = t;
             isect->hit = 1;
             
@@ -82,17 +119,17 @@ ray_sphere_intersect(Isect *isect, const Ray *ray, const Sphere *sphere)
     }
 }
 
-void
+__device__ void
 ray_plane_intersect(Isect *isect, const Ray *ray, const Plane *plane)
 {
-    double d = -vdot(plane->p, plane->n);
-    double v = vdot(ray->dir, plane->n);
+    float d = -vdot(plane->p, plane->n);
+    float v = vdot(ray->dir, plane->n);
 
-    if (fabs(v) < 1.0e-17) return;
+    if (fabs(v) < 1.0e-17f) return;
 
-    double t = -(vdot(ray->org, plane->n) + d) / v;
+    float t = -(vdot(ray->org, plane->n) + d) / v;
 
-    if ((t > 0.0) && (t < isect->t)) {
+    if ((t > 0.0f) && (t < isect->t)) {
         isect->t = t;
         isect->hit = 1;
         
@@ -104,20 +141,20 @@ ray_plane_intersect(Isect *isect, const Ray *ray, const Plane *plane)
     }
 }
 
-void
+__device__ void
 orthoBasis(vec *basis, vec n)
 {
     basis[2] = n;
-    basis[1].x = 0.0; basis[1].y = 0.0; basis[1].z = 0.0;
+    basis[1].x = 0.0f; basis[1].y = 0.0f; basis[1].z = 0.0f;
 
-    if ((n.x < 0.6) && (n.x > -0.6)) {
-        basis[1].x = 1.0;
-    } else if ((n.y < 0.6) && (n.y > -0.6)) {
-        basis[1].y = 1.0;
-    } else if ((n.z < 0.6) && (n.z > -0.6)) {
-        basis[1].z = 1.0;
+    if ((n.x < 0.6f) && (n.x > -0.6f)) {
+        basis[1].x = 1.0f;
+    } else if ((n.y < 0.6f) && (n.y > -0.6f)) {
+        basis[1].y = 1.0f;
+    } else if ((n.z < 0.6f) && (n.z > -0.6f)) {
+        basis[1].z = 1.0f;
     } else {
-        basis[1].x = 1.0;
+        basis[1].x = 1.0f;
     }
 
     vcross(&basis[0], basis[1], basis[2]);
@@ -127,60 +164,126 @@ orthoBasis(vec *basis, vec n)
     vnormalize(&basis[1]);
 }
 
-
-void ambient_occlusion(vec *col, const Isect *isect, Sphere *spheres, Plane plane)
+__device__ void
+ambient_occlusion(vec *col, const Isect *isect, RNG& rng, RNG_range& rng_range)
 {
+    int    i, j;
     int    ntheta = NAO_SAMPLES;
     int    nphi   = NAO_SAMPLES;
-    double eps = 0.0001;
+    float eps = 0.0001f;
 
-    vec p = {
-        isect->p.x + eps * isect->n.x,
-        isect->p.y + eps * isect->n.y,
-        isect->p.z + eps * isect->n.z};
+    vec p;
+
+    p.x = isect->p.x + eps * isect->n.x;
+    p.y = isect->p.y + eps * isect->n.y;
+    p.z = isect->p.z + eps * isect->n.z;
 
     vec basis[3];
     orthoBasis(basis, isect->n);
 
-    double occlusion = 0.0;
+    float occlusion = 0.0f;
 
-    for (int j = 0; j < ntheta; j++) {
-        for (int i = 0; i < nphi; i++) {
-            double theta = sqrt(drand48());
-            double phi   = 2.0 * M_PI * drand48();
+    for (j = 0; j < ntheta; j++) {
+        for (i = 0; i < nphi; i++) {
+            float theta = sqrt(rng_range(rng));
+            float phi   = 2.0f * (float)M_PI * rng_range(rng);
 
-            double x = cos(phi) * theta;
-            double y = sin(phi) * theta;
-            double z = sqrt(1.0 - theta * theta);
+            float x = cos(phi) * theta;
+            float y = sin(phi) * theta;
+            float z = sqrt(1.0f - theta * theta);
 
             // local -> global
-            double rx = x * basis[0].x + y * basis[1].x + z * basis[2].x;
-            double ry = x * basis[0].y + y * basis[1].y + z * basis[2].y;
-            double rz = x * basis[0].z + y * basis[1].z + z * basis[2].z;
+            float rx = x * basis[0].x + y * basis[1].x + z * basis[2].x;
+            float ry = x * basis[0].y + y * basis[1].y + z * basis[2].y;
+            float rz = x * basis[0].z + y * basis[1].z + z * basis[2].z;
 
-            Ray ray = {p, {rx, ry, rz}};
-            Isect occIsect = {1.0e+17, {0,0,0}, {0,0,0}, 0};
+            Ray ray;
+
+            ray.org = p;
+            ray.dir.x = rx;
+            ray.dir.y = ry;
+            ray.dir.z = rz;
+
+            Isect occIsect;
+            occIsect.t   = 1.0e+17f;
+            occIsect.hit = 0;
 
             ray_sphere_intersect(&occIsect, &ray, &spheres[0]); 
             ray_sphere_intersect(&occIsect, &ray, &spheres[1]); 
             ray_sphere_intersect(&occIsect, &ray, &spheres[2]); 
             ray_plane_intersect (&occIsect, &ray, &plane); 
 
-            if (occIsect.hit) occlusion += 1.0;
+            if (occIsect.hit) occlusion += 1.0f;
+            
         }
     }
 
-    occlusion = (ntheta * nphi - occlusion) / (double)(ntheta * nphi);
+    occlusion = (ntheta * nphi - occlusion) / (float)(ntheta * nphi);
 
     col->x = occlusion;
     col->y = occlusion;
     col->z = occlusion;
 }
 
-unsigned char
-clamp(double f)
+__global__ void
+dev_render(float *fimg, int w, int h, int nsubsamples)
 {
-  int i = (int)(f * 255.5);
+    int x = threadIdx.x;
+    int y = blockIdx.x;
+    unsigned int seed = y*w + x;
+
+    // seed a random number generator
+    RNG rng(seed);
+
+    // create a mapping from random numbers to [0,1)
+    RNG_range rng_range(0,1);
+
+    for (int v = 0; v < nsubsamples; v++) {
+        for (int u = 0; u < nsubsamples; u++) {
+            float px = (x + (u / (float)nsubsamples) - (w / 2.0f)) / (w / 2.0f);
+            float py = -(y + (v / (float)nsubsamples) - (h / 2.0f)) / (h / 2.0f);
+
+            Ray ray;
+
+            ray.org.x = 0.0f;
+            ray.org.y = 0.0f;
+            ray.org.z = 0.0f;
+
+            ray.dir.x = px;
+            ray.dir.y = py;
+            ray.dir.z = -1.0f;
+            vnormalize(&(ray.dir));
+
+            Isect isect;
+            isect.t   = 1.0e+17f;
+            isect.hit = 0;
+
+            ray_sphere_intersect(&isect, &ray, &spheres[0]);
+            ray_sphere_intersect(&isect, &ray, &spheres[1]);
+            ray_sphere_intersect(&isect, &ray, &spheres[2]);
+            ray_plane_intersect (&isect, &ray, &plane);
+
+            if (isect.hit) {
+                vec col;
+                ambient_occlusion(&col, &isect, rng, rng_range);
+
+                fimg[3 * (y * w + x) + 0] += col.x;
+                fimg[3 * (y * w + x) + 1] += col.y;
+                fimg[3 * (y * w + x) + 2] += col.z;
+            }
+
+        }
+    }
+
+    fimg[3 * (y * w + x) + 0] /= (float)(nsubsamples * nsubsamples);
+    fimg[3 * (y * w + x) + 1] /= (float)(nsubsamples * nsubsamples);
+    fimg[3 * (y * w + x) + 2] /= (float)(nsubsamples * nsubsamples);
+}
+
+unsigned char
+clamp(float f)
+{
+  int i = (int)(f * 255.5f);
 
   if (i < 0) i = 0;
   if (i > 255) i = 255;
@@ -188,59 +291,34 @@ clamp(double f)
   return (unsigned char)i;
 }
 
-
 void
-render(unsigned char *img, double* fimg, int w, int h, int nsubsamples, Sphere *spheres, Plane plane)
+render(unsigned char *img, int w, int h, int nsubsamples)
 {
-    int x, y;
-    int u, v;
+    float *fimg = (float *)malloc(sizeof(float) * w * h * 3);
+    float *d_fimg = NULL;
+    cutilSafeCall(cudaMalloc(&d_fimg, sizeof(float)*w*h*3));
 
-    for (y = 0; y < h; y++) {
-        for (x = 0; x < w; x++) {
-            for (v = 0; v < nsubsamples; v++) {
-                for (u = 0; u < nsubsamples; u++) {
-                    double px = (x + (u / (double)nsubsamples) - (w / 2.0)) / (w / 2.0);
-                    double py = -(y + (v / (double)nsubsamples) - (h / 2.0)) / (h / 2.0);
+    dev_render<<<w,h>>>(d_fimg, w, h, nsubsamples);
+    cutilCheckMsg("dev_render() execution failed\n");
 
-                    Ray ray = {
-                        {0.0, 0.0, 0.0},
-                        {px, py, -1.0}};
-
-                    vnormalize(&(ray.dir));
-
-                    Isect isect = {1.0e+17, {0,0,0}, {0,0,0}, 0};
-
-                    ray_sphere_intersect(&isect, &ray, &spheres[0]);
-                    ray_sphere_intersect(&isect, &ray, &spheres[1]);
-                    ray_sphere_intersect(&isect, &ray, &spheres[2]);
-                    ray_plane_intersect (&isect, &ray, &plane);
-
-                    if (isect.hit) {
-                        vec col;
-                        ambient_occlusion(&col, &isect, spheres, plane);
-
-                        fimg[3 * (y * w + x) + 0] += col.x;
-                        fimg[3 * (y * w + x) + 1] += col.y;
-                        fimg[3 * (y * w + x) + 2] += col.z;
-                    }
-
-                }
-            }
-
-            fimg[3 * (y * w + x) + 0] /= (double)(nsubsamples * nsubsamples);
-            fimg[3 * (y * w + x) + 1] /= (double)(nsubsamples * nsubsamples);
-            fimg[3 * (y * w + x) + 2] /= (double)(nsubsamples * nsubsamples);
+    cutilSafeCall(cudaMemcpy(fimg, d_fimg, sizeof(float)*w*h*3, cudaMemcpyDeviceToHost));
+    cutilSafeCall(cudaFree(d_fimg));
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
             img[3 * (y * w + x) + 0] = clamp(fimg[3 *(y * w + x) + 0]);
             img[3 * (y * w + x) + 1] = clamp(fimg[3 *(y * w + x) + 1]);
             img[3 * (y * w + x) + 2] = clamp(fimg[3 *(y * w + x) + 2]);
         }
     }
+    free(fimg);
 }
 
 void
 saveppm(const char *fname, int w, int h, unsigned char *img)
 {
-    FILE *fp = fopen(fname, "wb");
+    FILE *fp;
+
+    fp = fopen(fname, "wb");
     assert(fp);
 
     fprintf(fp, "P6\n");
@@ -253,17 +331,9 @@ saveppm(const char *fname, int w, int h, unsigned char *img)
 int
 main(int argc, char **argv)
 {
-    Sphere spheres[3] = {
-        {-2.0, 0.0, -3.5, 0.5},
-        {-0.5, 0.0, -3.0, 0.5},
-        {1.0, 0.0, -2.2, 0.5}};
-    Plane plane = {
-        {0.0, -0.5, 0.0},
-        {0.0, 1.0, 0.0}};
     unsigned char *img = (unsigned char *)malloc(WIDTH * HEIGHT * 3);
-    double *fimg = (double *)malloc(sizeof(double) * WIDTH * HEIGHT * 3);
 
-    render(img, fimg, WIDTH, HEIGHT, NSUBSAMPLES, spheres, plane);
+    render(img, WIDTH, HEIGHT, NSUBSAMPLES);
 
     saveppm("ao.ppm", WIDTH, HEIGHT, img); 
 
